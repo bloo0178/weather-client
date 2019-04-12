63c5763a2303880c071cdc731777c5fe82558a25
Jeff Bloom <bloo0178@gmail.com>
2019-04-11 20:57:13 -0400

    Changed background. Started restructuring app to accomodate loading spinners.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,6 +3,8 @@ import styles from "./App.module.css";
 import CurrentWeather from "../Components/CurrentWeather/CurrentWeather";
 import Forecast from "../Components/Forecast/Forecast";
 import LocationSearch from "../Components/LocationSearch/LocationSearch";
+import logo from "../common/stormLogo.svg";
+import { FadeLoader } from "react-spinners";
 
 class App extends Component {
   constructor(props) {
@@ -11,7 +13,8 @@ class App extends Component {
       location: "",
       locationName: "",
       units: "imperial", //offer ability to select
-      tempUnit: "F"
+      tempUnit: "F",
+      loading: false
     };
   }
 
@@ -22,11 +25,34 @@ class App extends Component {
     });
   };
 
+  isLoading = () => {
+    let loadState;
+    this.state.loading === true ? loadState = false : loadState = true;
+    this.setState({
+      loading: loadState
+    });
+  };
+
   render() {
-    const { location, locationName, units, tempUnit } = this.state;
+    const { location, locationName, units, tempUnit, loading } = this.state;
+    if (loading === true) {
+      return (
+        <div className={styles.Container}>
+          <div className={styles.Loading}>
+            <FadeLoader
+              sizeUnit={"px"}
+              size={150}
+              color={"#000000"}
+              loading={this.state.loading}
+            />
+          </div>
+        </div>
+      );
+    }
     if (location === "") {
       return (
         <div className={styles.Container}>
+          <img src={logo} className={styles.Logo} alt="logo" />
           <LocationSearch getLocation={this.getLocation} />
         </div>
       );
@@ -35,13 +61,12 @@ class App extends Component {
         <div className={styles.Container}>
           <LocationSearch getLocation={this.getLocation} />
           <h1>{locationName}</h1>
-
           <CurrentWeather
+            isLoading={this.isLoading}
             location={location}
             units={units}
             tempUnit={tempUnit}
           />
-
           <div>
             <Forecast location={location} units={units} tempUnit={tempUnit} />
           </div>
